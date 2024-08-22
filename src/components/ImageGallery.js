e467a00214794c1f28115a902eaa56fc77e4205b
Sebastian Navarro <gsebasncoder@gmail.com>
2024-08-22 16:35:29 -0300

    Refactor - Remove toggle feature from main view.

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -11,13 +11,59 @@ const initialImages = [
     {
         low: 'https://ucarecdn.com/85401fc8-ce16-45a8-a849-c28b27b31b01/-/scale_crop/300x300/',
         high: 'https://ucarecdn.com/85401fc8-ce16-45a8-a849-c28b27b31b01/-/preview/666x1000/',
-    }
+    },
+    {
+        low: 'https://ucarecdn.com/85401fc8-ce16-45a8-a849-c28b27b31b01/-/scale_crop/300x300/',
+        high: 'https://ucarecdn.com/85401fc8-ce16-45a8-a849-c28b27b31b01/-/preview/666x1000/',
+    },
+    {
+        low: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/scale_crop/300x300/',
+        high: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/preview/999x669/',
+    },
+    {
+        low: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/scale_crop/300x300/',
+        high: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/preview/664x1000/',
+    },
+    {
+        low: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/scale_crop/300x300/',
+        high: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/preview/999x669/',
+    },
+    {
+        low: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/scale_crop/300x300/',
+        high: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/preview/664x1000/',
+    },
+    {
+        low: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/scale_crop/300x300/',
+        high: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/preview/999x669/',
+    },
+    {
+        low: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/scale_crop/300x300/',
+        high: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/preview/664x1000/',
+    },
+    {
+        low: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/scale_crop/300x300/',
+        high: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/preview/999x669/',
+    },
+    {
+        low: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/scale_crop/300x300/',
+        high: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/preview/664x1000/',
+    },
+    {
+        low: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/scale_crop/300x300/',
+        high: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/preview/999x669/',
+    },
+    {
+        low: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/scale_crop/300x300/',
+        high: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/preview/664x1000/',
+    },
     // Add more initial images as needed
 ];
 
 const ImageGallery = () => {
     const [images, setImages] = useState(initialImages);
     const [page, setPage] = useState(1);
+    const [currentImageIx, setCurrentImageIx] = useState(0);
+    const [fullViewMode, setFullViewMode] = useState(false);
     const { ref, inView } = useInView({
         triggerOnce: false,
         threshold: 0.5,
@@ -25,12 +71,8 @@ const ImageGallery = () => {
 
     const { theme, setTheme } = useContext(GalleryContext);
 
-    const goToFullMode = () => {
-        if (theme === '') {
-           setTheme('grid');
-        } else {
-            setTheme('');
-        }
+    const goToFullMode = (index) => {
+        setFullViewMode(!fullViewMode);
     }
 
     const loadMoreImages = useCallback(() => {
@@ -39,18 +81,6 @@ const ImageGallery = () => {
 
         // Simulate an API call to fetch more images
         const newImages = [
-            {
-                low: 'https://ucarecdn.com/85401fc8-ce16-45a8-a849-c28b27b31b01/-/scale_crop/300x300/',
-                high: 'https://ucarecdn.com/85401fc8-ce16-45a8-a849-c28b27b31b01/-/preview/666x1000/',
-            },
-            {
-                low: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/scale_crop/300x300/',
-                high: 'https://ucarecdn.com/5f08de9e-2b32-4ec1-a003-e20b25294f92/-/preview/999x669/',
-            },
-            {
-                low: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/scale_crop/300x300/',
-                high: 'https://ucarecdn.com/97161757-5b6b-4553-a438-c0b6bf6156a0/-/preview/664x1000/',
-            },
             // Add more new images as needed
         ];
         setImages((prevImages) => [...prevImages, ...newImages]);
@@ -66,21 +96,42 @@ const ImageGallery = () => {
     return (
         <>
             <div className="gallery-content">
-                <div className={`gallery ${theme}`} >
-                    {
-                        images.map((image, index) => (
-                            <div className="gallery-item" key={index} onClick={goToFullMode}>
-                                <LazyImage src={image.high}
-                                           placeholderSrc={image.low}
-                                           alt={`Gallery image ${index + 1}`}
-                                />
+                { !fullViewMode &&
+                    <div>
+                        <div className={`gallery grid`}>
+                            {
+                                images.map((image, index) => (
+                                    <div className="gallery-item"
+                                         key={index}
+                                         onClick={() => goToFullMode(index)}
+                                    >
+                                        <LazyImage src={image.high}
+                                                   placeholderSrc={image.low}
+                                                   alt={`Gallery image ${index + 1}`}
+                                        />
+                                    </div>
+                                ))
+                            }
+                            <div ref={ref} className="loading">
+                                Loading more images...
                             </div>
-                        ))
-                    }
-                    <div ref={ref} className="loading">
-                        Loading more images...
+                        </div>
+                    </div>
+                }
+                { fullViewMode &&
+                <div>
+                    <div className={`gallery-item`}
+                         onClick={() => goToFullMode()}
+                    >
+                        {
+                            <LazyImage src={images[currentImageIx].high}
+                                placeholderSrc={images[currentImageIx].low}
+                                alt={`full image ${currentImageIx + 1}`}
+                            />
+                        }
                     </div>
                 </div>
+                }
             </div>
         </>
     );
